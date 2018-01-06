a05446321a8509e05393555f71c0ae2348371d20
tomek <tomasz.nastaly@gmail.com>
2018-01-06 16:44:56 +0100

    renderowanie niezależne od rozmiaru widoku, żaba skacze

--- app/game_model.py
+++ app/game_model.py
@@ -4,6 +4,8 @@ from app.models.player import Player
 class GameModel:
     def __init__(self):
         self.player = Player()
+        self.ref_height = 0.15
+        self.ref_width = 0.1
 
     def update(self):
         self.player.update()
--- app/models/game_object.py
+++ app/models/game_object.py
@@ -4,13 +4,15 @@ from abc import ABC, abstractmethod
 
 class GameObject(ABC):
 
-    def __init__(self, location, strategy):
-        self.__location = location
-        self.__strategy = strategy
-        self.__id = uuid.uuid4()
+    def __init__(self, x_pos, y_pos, width, height):
+        self.id = uuid.uuid4()
+        self.width = width
+        self.height = height
+        self.x = x_pos
+        self.y = y_pos
 
     def get_size(self):
-        return 1
+        return self.width, self.height
 
     @abstractmethod
     def get_name(self):
